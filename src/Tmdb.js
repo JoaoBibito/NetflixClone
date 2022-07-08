a037d054980208791d63542a56d303e24d6c10ca
JoaoBibito <joao-vitor1903@live.com>
2022-07-08 07:16:38 -0300

    Page series

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -78,5 +78,35 @@ export default{
             }
         }
         return info;
+    },   
+    getSeries: async()=>{//genre/tv/list?api_key=${api_key}&language=pt-BR
+        let list= await basicFetch(`/discover/tv?api_key=${api_key}&language=pt-BR`)
+        return [
+            {
+                slug:"Series",
+                title:"Series",
+                items:await basicFetch(`/discover/tv?api_key=${api_key}&language=pt-BR`)
+            },
+            {
+                slug:"Comedy",
+                title:"Comédia",
+                items:await basicFetch(`/discover/tv?with_genres=35&language=pt-BR&api_key=${api_key}`)
+            },
+            {
+                slug:"Animation",
+                title:"Animação ",
+                items:await basicFetch(`/discover/tv?with_genres=16&language=pt-BR&api_key=${api_key}`)
+            },
+            {
+                slug:"Kids",
+                title:"Infantil",
+                items:await basicFetch(`/discover/tv?with_genres=10762&language=pt-BR&api_key=${api_key}`)
+            },
+            {
+                slug:"Drama",
+                title:"Drama",
+                items:await basicFetch(`/discover/tv?with_genres=18&language=pt-BR&api_key=${api_key}`)
+            }
+        ];
     }
 }
--- src/pages/series/index.jsx
+++ src/pages/series/index.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import './index.css';
 import Header from '../../components/Header';
+import FeaturedMovie from '../../components/FeaturedMovie';
+import Tmdb from '../../Tmdb';
+import MovieRow from '../../components/MovieRow';
 export default ()=>{
+
+    let page ='series';
+    const [featuredData,setFeaturedData]=useState(null);   
+    const [blackHeader,setBlackHeader] = useState(false);
+    const [serieList,setSerieList] = useState([]);
+
+    useEffect(()=>{
+        const loadAll = async()=>{
+            //get films list
+            let list  = await Tmdb.getSeries();
+            setSerieList(list);
+
+            //getFeatured
+        }
+        loadAll();
+    },[])
+
     return (
-        <Header/>        
+        <div className='page'>
+            <Header black={blackHeader} page={page}/> 
+            {featuredData && <FeaturedMovie item={featuredData}/>}
+            <section className='seriesList'>
+                {serieList.map((item,key)=>(
+                    <MovieRow key={key} title={item.title} items={item.items}/>
+                ))}
+            </section>
+            {serieList.length <=0 &&
+            <div className='loading'>
+                <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando"/>
+            </div>}
+       </div>       
     )
 }
