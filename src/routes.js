377c6f7e98c195809d57f01fe006da816e8df998
JoaoBibito <joao-vitor1903@live.com>
2022-08-09 13:39:43 -0300

    delete readme

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ import Latest from './pages/latest';
 
 function AppRoutes(){
   return (
-    <HashRouter >
+    <HashRouter baseline="">
     <Routes>
         <Route path='/' exact element={<Index/>}/>
         <Route path='/Series' exact element={<Series/>}/>
